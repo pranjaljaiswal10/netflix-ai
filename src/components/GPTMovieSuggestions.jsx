07423bab4388ca8ef6dffddc9d34f16f9dbe7e8c
Pranjal Jaiswal <pranjaljaiswal35@gmail.com>
2024-03-05 22:11:47 +0530

    refactor:remove unnecessary file

--- src/components/GPTMovieSuggestions.jsx
+++ src/components/GPTMovieSuggestions.jsx
@@ -3,8 +3,7 @@ import useGPTSearch from "../utils/hooks/useGPTsearch"
 
 const GPTMovieSuggestions = () => {
   const [searchTxt,setSearchTxt]=useState("")
-
-  
+ 
   return (<>
   <input type="text" placeholder="" value={searchTxt} onChange={(e)=>setSearchTxt(e.target.value)} />
   <button>Search</button>
